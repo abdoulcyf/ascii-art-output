283a2e8ccf685ffb7e5cb44acdd6b11482694b45
ediallo <elhhabdouldiallo@gmail.com>
2024-04-25 17:19:44 +0100

    replaces getFianlStr.go with bannerToStr.go

---
+++ pkg/cli/bannerToStr.go
@@ -0,0 +1,41 @@
+package cli
+
+import (
+	"errors"
+	"os"
+
+	"github.com/ediallocyf/output/pkg/util"
+)
+
+// BannerToStr converts the received string into its asccii char equivalent
+func BannerToStr() (string, error) {
+	funcName = " --<--BannerToStr--<--"
+	// get the second cliStr for reading the specified banner
+	args := os.Args
+	chosenBanner := args[3]
+	//----------------------------------------
+	patternContent, errPattern := util.ReadFileToStr(directory, chosenBanner, fileExtention)
+
+	if errPattern != nil {
+		funcDirectLink = "ReadFileToStr--<-- "
+		funcErrorMsg = "Error : loading pattern file failed:"
+		logger.Error(funcErrorMsg + funcName + funcDirectLink + errPattern.Error())
+		return "", errors.New(errPattern.Error())
+	}
+	//--------------------------------
+	patternMap, errPatternMap := util.ContentToMap(patternContent, chLength)
+
+	if errPatternMap != nil {
+		return "", errors.New(errPatternMap.Error())
+	}
+	//--------------------------------------------
+	cliStr, errClistr := util.ReadCli()
+
+	if errClistr != nil {
+		return "", errClistr
+	}
+	//----------------------------------------------
+
+	finalStr := util.MapToStr(cliStr, patternMap, chLength)
+	return finalStr, nil
+}
